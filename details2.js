1952a5cee036e0f94b28be0ec6e7014d361ea0f5
BLAMISS <besbes.lamiss@yahoo.Fr>
2023-09-23 03:49:17 +0200

    l

--- details2.js
+++ details2.js
@@ -1,20 +1,19 @@
 <script>
 document.addEventListener("DOMContentLoaded", function () {
-  // Check if the user is logged in (you should implement this logic)
-  var isLoggedIn = false; // Replace with your actual authentication logic
+  var isLoggedIn = false; 
 
-  // Get a reference to the "Details" button
+
   var detailsButton = document.getElementById("details2");
 
-  // Add a click event listener to the "Details" button
+  
   detailsButton.addEventListener("click", function () {
     if (isLoggedIn) {
-      // If the user is logged in, redirect to the protected page
+      
       window.location.href = "drawing_plan.html";
     } else {
-      // If the user is not logged in, redirect to the login page
+      
       window.location.href = "login.html";
     }
-  });
+});
 });
 </script>
